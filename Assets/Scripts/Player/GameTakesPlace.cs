635cf540b6da48df0131461617af46b8845b7bb7
Nico Novikov <19169221+mnov23@users.noreply.github.com>
2025-12-08 16:43:34 +0200

    Happy New Year and Merry Christmas in advance! (#6)
    * added few more assets
    
    another unproductive commit. I wish I could tackle the issue that bogged me down in November. Once and for all.
    
    * 3rd blog-posting. Feedback.
    
    no progress, nothing to show.

---
+++ Assets/Scripts/Player/GameTakesPlace.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Player
+{
+    internal class GameTakesPlace : MonoBehaviour
+    {
+        // simulate victory
+        // simulate loss
+        // return back... to main menu cause why not.
+
+        // practicing ad integration.
+
+        // do the OnClicks ()
+        // see toilet_game_manager.cs   for more code
+        // see temporary for more scripts
+    }
+}
